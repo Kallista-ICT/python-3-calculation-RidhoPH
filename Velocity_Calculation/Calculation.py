cd377c1fb86480e7d943e024867216125f190d37
RidhoPH <ridho.pratomo2305@gmail.com>
2025-02-05 01:48:30 +0000

    Submit Ridho

---
+++ Velocity_Calculation/Calculation.py
@@ -0,0 +1,9 @@
+#Input distance and time
+Distance = float(input("Enter the distance (in meters): "))
+Time = float(input("Enter the time (in seconds): "))
+
+#Formula of Velocity
+Velocity = Distance/Time
+
+#Print the Velocity 
+print(f"the velocity is {Velocity:.2f} m/s.")
